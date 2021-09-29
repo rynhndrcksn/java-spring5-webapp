6b38db5f8f5d7bff4d5ed97c29b99cc1f0934df0
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2021-09-29 02:05:15 -0700

    Created starting Spring project

---
+++ src/test/java/edu/greenriver/sdev/myspringproject/MySpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.myspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
