16c8006a5d13559e11d948c7b870fa9d67820bad
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2021-10-26 03:09:12 -0700

    Moved SQL queries to data.sql (debugging)

--- src/main/java/edu/greenriver/sdev/myspringproject/MySpringProjectApplication.java
+++ src/main/java/edu/greenriver/sdev/myspringproject/MySpringProjectApplication.java
@@ -29,38 +29,6 @@ public class MySpringProjectApplication {
 
 		// get the data layer bean
 		IJobRepo jobRepo = context.getBean(IJobRepo.class);
-
-		// add some jobs
-		Job[] jobs = {
-				Job.builder().name("Job 1")
-						.employer("Employer 1")
-						.startDate(LocalDate.now())
-						.endDate(null)
-						.description("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis tincidunt id aliquet risus.")
-						.build(),
-				Job.builder().name("Job 2")
-						.employer("Employer 2")
-						.startDate(LocalDate.of(2020, 8, 20))
-						.endDate(LocalDate.of(2020, 9, 1))
-						.description("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis tincidunt id aliquet risus.")
-						.build(),
-				Job.builder().name("Job 3")
-						.employer("Employer 3")
-						.startDate(LocalDate.of(2019, 3, 15))
-						.endDate(LocalDate.of(2020, 8, 2))
-						.description("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis tincidunt id aliquet risus.")
-						.build(),
-				Job.builder().name("Job 4")
-						.employer("Employer 4")
-						.startDate(LocalDate.of(2019, 1, 1))
-						.endDate(LocalDate.of(2019, 3, 10))
-						.description("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis tincidunt id aliquet risus.")
-						.build()
-		};
-
-		// save all the jobs to our DB
-		jobRepo.saveAll(Arrays.asList(jobs));
-		System.out.println("All jobs saved to the DB");
 	}
 
 }
