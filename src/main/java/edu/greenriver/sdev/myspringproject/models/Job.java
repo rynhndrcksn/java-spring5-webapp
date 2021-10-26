1244cc0e33674ed617a1c416087a7a6a6939b777
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2021-10-26 03:22:28 -0700

    forced jpa to use specific column names for start/end date.

--- src/main/java/edu/greenriver/sdev/myspringproject/models/Job.java
+++ src/main/java/edu/greenriver/sdev/myspringproject/models/Job.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
@@ -29,7 +26,11 @@ public class Job implements Comparable<Job> {
 
 	private String name;
 	private String employer;
+
+	@Column(name = "start_date")
 	private LocalDate startDate;
+
+	@Column(name = "end_date")
 	private LocalDate endDate;
 	private String description;
 
